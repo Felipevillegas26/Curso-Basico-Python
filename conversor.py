2bf517b63b1b5b3a7dcad081e3a7bca5801dc6f1
Julian Villegas <julian.villegas@bluetab.net>
2025-08-13 07:38:44 -0500

    cambios finales

---
+++ conversor.py
@@ -0,0 +1,12 @@
+temperatura = float(input("Ingrese Temperatura:"))
+escala = input("Es Fahrenheit(F) o es Celsius(C)?:").lower()
+
+if escala == "f":
+    celsiusc = (temperatura - 32) * 5/9
+    print(celsiusc)
+elif escala == "c":
+    fahrenheit = temperatura * 1.8 * 32
+    print(fahrenheit)
+else:
+    print("Escala Incorrecta")
+
